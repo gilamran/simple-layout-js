ae68629690c4efab5eb3ce3dfb09ccca88291ace
gil <gil@wix.com>
2014-10-21 14:52:43 +0300

    remove the IManagedAssetsFactory

--- src/simpleLayout/assetsFactory/IManagedAssetsFactory.ts
+++
@@ -1,5 +0,0 @@
-interface IManagedAssetsFactory extends IAssetsFactory {
-    loadAssets(doneCallback:() => void, errorCallback:(errorMessage:string) => void, progressCallback:(percentDone:number) => void):void;
-    getAssetsIds():string[];
-    disposeAssets():void;
-}
